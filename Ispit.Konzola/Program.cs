72d49c3e3740ab13e71a50ec1ffb304e0ffb1553
ftesija <tesijafilip@gmail.com>
2024-06-07 19:24:18 +0200

    Add project files.

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,65 @@
+﻿using Ispit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucenik ucenik1 = new Ucenik();
+            Console.WriteLine("Upišite ime učenika: ");
+            ucenik1.Ime = Console.ReadLine();
+            Console.WriteLine("Upišite prezime učenika: ");
+            ucenik1.Prezime = Console.ReadLine();
+            Console.WriteLine("Upišite datum rođenja učenika(MM-dd-yyyy): ");
+            ucenik1.DatumRodjenja = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Upišite prosjek učenika: ");
+            ucenik1.Prosjek = Convert.ToDouble(Console.ReadLine());
+
+            Ucenik ucenik2 = new Ucenik();
+            Console.WriteLine("Upišite ime učenika: ");
+            ucenik2.Ime = Console.ReadLine();
+            Console.WriteLine("Upišite prezime učenika: ");
+            ucenik2.Prezime = Console.ReadLine();
+            Console.WriteLine("Upišite datum rođenja učenika(MM-dd-yyyy): ");
+            ucenik2.DatumRodjenja = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Upišite prosjek učenika: ");
+            ucenik2.Prosjek = Convert.ToDouble(Console.ReadLine());
+
+            Ucenik ucenik3 = new Ucenik();
+            Console.WriteLine("Upišite ime učenika: ");
+            ucenik3.Ime = Console.ReadLine();
+            Console.WriteLine("Upišite prezime učenika: ");
+            ucenik3.Prezime = Console.ReadLine();
+            Console.WriteLine("Upišite datum rođenja učenika(MM-dd-yyyy): ");
+            ucenik3.DatumRodjenja = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Upišite prosjek učenika: ");
+            ucenik3.Prosjek = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine($"Ime: {ucenik1.Ime}");
+            Console.WriteLine($"Ime: {ucenik1.Prezime}");
+            ucenik1.Starost();
+            ucenik1.IspisProsjeka();
+            
+            Console.WriteLine($"Ime: {ucenik2.Ime}");
+            Console.WriteLine($"Ime: {ucenik2.Prezime}");
+            ucenik2.Starost();
+            ucenik2.IspisProsjeka();
+
+            Console.WriteLine($"Ime: {ucenik3.Ime}");
+            Console.WriteLine($"Ime: {ucenik3.Prezime}");
+            ucenik3.Starost();
+            ucenik3.IspisProsjeka();
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Ucenik 
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+
+        public double Prosjek { get; set; }
+
+
+        public int Starost() 
+        {
+            DateTime upravo = DateTime.Today;
+            int godine = upravo.Year - DatumRodjenja.Year;
+            Console.WriteLine($"Ucenik ima {godine} godina.");
+            return godine;
+            
+            
+        }
+
+        
+
+        public void IspisProsjeka() 
+        {
+            double ocjena = Prosjek;
+
+            if (ocjena > 1.0 && ocjena <= 1.49)
+            {
+                Console.WriteLine("Ocjena je nedovoljan!");
+            }
+            else if (ocjena >= 1.5 && ocjena <= 2.49)
+            {
+                Console.WriteLine("Ocjena je dovoljan!");
+            }
+            else if (ocjena >= 2.5 && ocjena <= 3.49)
+            {
+                Console.WriteLine("Ocjena je dobar!");
+            }
+            else if (ocjena >= 3.5 && ocjena <= 4.49)
+            {
+                Console.WriteLine("Ocjena je vrlo dobar!");
+            }
+            else if (ocjena >= 4.5 && ocjena <= 5.0)
+            {
+                Console.WriteLine("Ocjena je odličan!");
+            }
+            else 
+            {
+                Console.WriteLine("Krivi unos!");
+            }
+        }
+
+    }
+}
